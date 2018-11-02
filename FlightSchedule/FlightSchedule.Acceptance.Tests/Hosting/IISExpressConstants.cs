bf37d0947ff5a5d75d8874ef125ff100c229ef83
Class-2-Teacher <Class-2-Teacher@CLASS1-TEACHER>
2018-11-02 16:12:57 +0330

    automatic hosting sut

---
+++ FlightSchedule/FlightSchedule.Acceptance.Tests/Hosting/IISExpressConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace FlightSchedule.Acceptance.Tests.Hosting
+{
+  
+    public static class IISExpressConstants
+    {
+        public readonly static string path = Path.GetPathRoot(Environment.SystemDirectory);
+        //path install windows
+        public readonly static string IISExpressX86Path = path.Replace(@"\", "") +  @"\Program Files (x86)\IIS Express\iisexpress.exe"; 
+        public const string IISExpressPath = @"C:\Program Files\IIS Express\iisexpress.exe";
+    }
+}
